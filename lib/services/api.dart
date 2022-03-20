630f6fb5f3166b5cb6461fd632c1e87937db947b
kimkyungwon <openwind125@gmail.com>
2022-03-20 23:35:17 +0900

    Add getCocktailDataById function

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -13,3 +13,13 @@ Future<Cocktail> getCocktailData() async {
     throw Exception("Error loading news");
   }
 }
+
+Future<Cocktail> getCocktailDataById({required String id}) async {
+  final response = await get(Uri.parse(
+      'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=$id'));
+  if (response.statusCode == 200) {
+    return Cocktail.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception("Error loading news");
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,14 +3,19 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() async {
   var data = await getCocktailData();
+  var idData = await getCocktailDataById(id: '11007');
   print(data.drinks?[0].strDrink);
   print(data.drinks?[0].idDrink);
 
   test('first', () {
-    expect('155 Belmont', data.drinks?[0].strDrink);
+    expect(data.drinks?[0].strDrink, '155 Belmont');
   });
 
   test('id check', () {
-    expect('15346', data.drinks?[0].idDrink);
+    expect(data.drinks?[0].idDrink, '15346');
+  });
+
+  test('get data by id', () {
+    expect(idData.drinks?[0].strDrink, 'Margarita');
   });
 }
