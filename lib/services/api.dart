65eb13355d40acbea5121ef623ade9513b23dbb5
kimkyungwon <openwind125@gmail.com>
2022-03-21 17:10:12 +0900

    Update url

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -25,7 +25,7 @@ Future<Cocktail> getCocktailDataById({required String id}) async {
 
 Future<Cocktail> getCocktailDataByInputText({required String inputText}) async {
   final response = await get(Uri.parse(
-      'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=$inputText'));
+      'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=$inputText'));
   if (response.statusCode == 200) {
     return Cocktail.fromJson(jsonDecode(response.body));
   } else {
